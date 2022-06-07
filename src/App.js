18d9d0f32d99d06dd6b3a22ab44bcd9ea892be26
Rohit Bhaud <92250506+RohitBhaud@users.noreply.github.com>
2022-06-07 11:28:13 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TicTacTow from './Components/TicTacTow/TicTacTow';
+
+function App() {
+  return (
+    <div className="App">
+     <TicTacTow/>
+    </div>
+  );
+}
+
+export default App;
